cd87e9066a1b3c2c38dc59e1af24113ffbbe310a
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2023-12-22 02:26:06 -0300

    card de dc terminada owo

--- src/componets/Tateti.js
+++ src/componets/Tateti.js
@@ -15,6 +15,7 @@ function Tateti(){
                         border-radius={50}
                         playing
                         loop
+                        //volume={0.3}
                         muted
                     />
                 </div>
@@ -23,19 +24,34 @@ function Tateti(){
                 
                 <section className='general'>
                     <div className='datos'>
-                        <h2> &#8475;oma</h2>
-                        <p>@sprentina</p>
+                        <h1 className='nombre'> &#x211C;oma</h1>
+                        <p>sprentina</p>
                         <p>She/her</p>
                     </div>
                     <div className='datos2'>
-                        <h3>Sobre Mi</h3>
+                        <h3 className='subTittle'>Sobre Mi</h3>
                         <p>barra brava  del club atlético newjeans</p>
                     </div>
+                    {/* <div className='datos3'>
+                        <h3 className='subTittle'>Miembro Desde</h3>
+                        <p>&#127827; 23 Oct 22</p>
+                    </div> */}
                     <div className='datos3'>
-                        <h3>Miembro Desde</h3>
-                        <p> 23 Oct 22</p>
+                        <h3 className='subTittle'>Escuchando</h3>
+                        <div className='reproductor'>
+                            <section className="musica"></section>
+                            <div className='info'>
+                                <p className='cosito'>Super Shy</p>
+                                <p>de New Jeans</p>
+                                <p>en NewJeans 2nd EP 'Get Up'</p>
+                            </div>
+                        </div>
+                        <section className='numeritos'>
+                            <p>00:00</p>
+                            <p>02:34</p>
+                        </section>
                     </div>
-                    <h3>Roles</h3>
+                    <h3 className='subTittle'>Roles</h3>
                     <div className='roles'>
                         <div className='rolsito'> <div className='bolita' id='b1'></div> &#10098;&#9733;&#10099; romita</div>
                         <div className='rolsito'> <div className='bolita' id='b2'></div> &#10098;&#x1F49A;&#10099; SpreenUpd</div>
