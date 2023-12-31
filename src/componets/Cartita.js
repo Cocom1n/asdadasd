8c4a10fef1df408da57b2cf309359192c6da9ddc
Carrasco <136844452+Cocom1n@users.noreply.github.com>
2023-12-31 00:53:12 -0300

    chat base

--- src/componets/Cartita.js
+++ src/componets/Cartita.js
@@ -7,13 +7,18 @@ function Cartita(){
             <section className="palabras">
                 <div className="yomensaje">
                     <section id="yopfp"></section>
-                    <h1 className="chat">min</h1>
+                    <h1 className="yoo">Min</h1>
                 </div>
                 <div className="chat">
                     <p className="msg">Feliz navidad y feliz año nuevo!!</p>
                     <p className="msg">Espero que hayas pasado unas lindas fiestas y este tengas un muy buen 2024</p>
                     <p className="msg">Tqm &#x1F49C;</p>
-                    <p className="msg"></p>
+                    <p className="msg">Aunque ya no estes con los papus siempre vas a ser la mejor papuresumera de resumenes :'v</p>
+                    <p className="msg">como como como me pregunta el man</p>
+                    <p className="msg">mal</p>
+                    <p className="msg">me lo puse a pensar</p>
+                    <p className="msg">como como como si como normal</p>
+                    <p className="msg">&#x1F49C;&#x1F49C;&#x1F49C;&#x1F49C;</p>
                 </div>     
                 
 
